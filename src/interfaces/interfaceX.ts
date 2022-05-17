1c01036efb9d1a82f14912545c29f76b0ef3c32f
glener10 <glenerpizzolato@gmail.com>
2022-05-17 10:13:35 -0300

    Definindo arquitetura

---
+++ src/interfaces/interfaceX.ts
@@ -0,0 +1,16 @@
+/*export enum ESTADO_PROCESSO_ENUM {
+  ANDAMENTO = "A",
+  EXTINTO = "E",
+  PENDENTE = "P",
+  SUSPENSO = "S",
+}
+export interface IProcesso {
+  codProcesso: number;
+  numProcesso: number;
+  dthAbertura: Date;
+  codPessoaCliente: number;
+  codAcao: number;
+  observacao: string;
+  estadoProcesso: ESTADO_PROCESSO_ENUM;
+}
+*/
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View } from 'react-native';
+import Login from './Login';
+
+export default function App() {
+  return (
+    <Login />
+  );
+}
