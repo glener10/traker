8320d212496d550d553d479398376454a0858376
glener10 <glenerpizzolato@gmail.com>
2022-05-25 18:15:37 -0300

    update files

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,28 @@
 import axios from 'axios';
+//import { getCookiePromisse } from "../utils/coockies";
+
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
-  headers: { "Acess-Control-Allow-Origin": "*" },
-  secure: false,
-  changeOrigin: true,
+  baseURL: 'http://localhost:3000',
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
+/*api.interceptors.request.use(
+  async (config) => {
+    var token = await getCookiePromisse("kcIdToken");
+    if (token) {
+      var decodedToken = Buffer.from(String(token), "base64").toString(
+        "binary"
+      );
+      config.headers!["Authorization"] = "Bearer " + decodedToken;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);*/
 
 export default api;
