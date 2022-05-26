674bc2328f9e9e67fe18d9fb204e855533ead43f
Igor Capeletti <igorcapeletti.aluno@unipampa.edu.br>
2022-05-25 21:01:54 -0300

    att

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { Text } from 'react-native';
+import { ImageBackground , StyleSheet, View, Text } from 'react-native';
 import api from '../../services/api';
 
 
 function Login() {
-  const [users, setUsers] = useState([]);
+  /*const [users, setUsers] = useState([]);
 
   useEffect(() => {
     async function getUsers() {
@@ -15,14 +15,32 @@ function Login() {
       }
       catch {
         alert("Erro ao realizar get");
+        console.log("Alô");
       }
     }
     getUsers();
   }, []);
+  */
 
   return (
-    <Text>Eu sou o login</Text>
+    <View style={styles.container}>
+      <Text>Eu sou o login sasdad</Text>
+      <Text>Testando</Text>
+      <ImageBackground  
+        source={{uri: 'https://github.com/igor-capeletti.png'}} 
+        style={{width: 400, height: 400}}  
+      />
+    </View>
   );
 }
+
+const styles= StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    alignContent: 'center',
+  },
+});
 
 export default Login;
