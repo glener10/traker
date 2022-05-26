bd46c82220aed9db394598f05d856314cb190cd0
Igor Capeletti <igorcapeletti.aluno@unipampa.edu.br>
2022-05-25 21:58:05 -0300

    att login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,46 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import { ImageBackground , StyleSheet, View, Text } from 'react-native';
-import api from '../../services/api';
-
+import React from 'react';
+import {ImageBackground, View, Text} from 'react-native';
+import {stylesLogin} from './loginStyle';
 
 function Login() {
-  /*const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    async function getUsers() {
-      try {
-        const { data } = await api.get("/employees");
-        console.log(data);
-        setUsers(data);
-      }
-      catch {
-        alert("Erro ao realizar get");
-        console.log("Alô");
-      }
-    }
-    getUsers();
-  }, []);
-  */
-
   return (
-    <View style={styles.container}>
+    <View style={stylesLogin.container}>
       <Text>Eu sou o login sasdad</Text>
       <Text>Testando</Text>
-      <ImageBackground  
-        source={{uri: 'https://github.com/igor-capeletti.png'}} 
-        style={{width: 400, height: 400}}  
+      <ImageBackground
+        source={{uri: 'https://github.com/igor-capeletti.png'}}
+        style={{
+          width: 200,
+          height: 200,
+          borderColor: 'black',
+        }}
       />
     </View>
   );
 }
-
-const styles= StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    alignContent: 'center',
-  },
-});
 
 export default Login;
