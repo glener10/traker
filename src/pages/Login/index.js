755e0ec1261b74a03600d2c182a04e47c804fa9b
glener10 <glenerpizzolato@gmail.com>
2022-06-01 22:45:15 -0300

    Adiciona Button

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import {Container, Form} from 'react-bootstrap';
-import {View, Image} from 'react-native';
-import {stylesLogin} from './loginStyle';
+//TODO: Importação do Button é em react-native mas outras props vem do react-bootstrap
+import { View, Image } from 'react-native';
+import { stylesLogin } from './loginStyle';
 
 function Login() {
   return (
     <View style={stylesLogin.container}>
-      {
+      <View>
         <Image
           source={require('../../assets/logo_app.png')}
           style={stylesLogin.logo}
         />
-      }
+
+      </View>
+
+      <View>
+        <Button title="EU SOU UM BOTÃO"></Button>
+      </View>
     </View>
   );
 }
