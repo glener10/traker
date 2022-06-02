90067cf7f664056551897bf0bb6eff8d5a701e76
Igor Capeletti <igorcapeletti.aluno@unipampa.edu.br>
2022-06-02 01:01:30 -0300

    att

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,28 +1,17 @@
 import React from 'react';
-import {Container, Form, Button} from 'react-bootstrap';
+import {Container, Form} from 'react-bootstrap';
 import {View, Image} from 'react-native';
 import {stylesLogin} from './loginStyle';
 
 function Login() {
   return (
     <View style={stylesLogin.container}>
-      <Container>
-        {<Image source={require('../../assets/logo_app.png')} />}
-      </Container>
-      <Form>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Usuário</Form.Label>
-          <Form.Control type="email" placeholder="" />
-        </Form.Group>
-
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Senha</Form.Label>
-          <Form.Control type="password" placeholder="" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Logar
-        </Button>
-      </Form>
+      {
+        <Image
+          source={require('../../assets/logo_app.png')}
+          style={stylesLogin.logo}
+        />
+      }
     </View>
   );
 }
--- src/pages/Login/loginStyle.js
+++ src/pages/Login/loginStyle.js
@@ -1,10 +1,17 @@
-import {  StyleSheet } from 'react-native';
+import {StyleSheet} from 'react-native';
+import {any} from 'react-native/Libraries/Text/TextNativeComponent';
 
-export const stylesLogin= StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: 'white',
-      alignItems: 'center',
-      alignContent: 'center',
-    },
-  });
+export const stylesLogin = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    alignContent: 'center',
+  },
+  logo: {
+    flex: 0,
+    width: 200,
+    aspectRatio: 1.0,
+    justifyContent: 'space-around',
+  },
+});
